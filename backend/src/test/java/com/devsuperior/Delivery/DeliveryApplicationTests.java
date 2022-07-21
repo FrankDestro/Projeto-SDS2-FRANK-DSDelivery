c8a3857f6644ec4d757ed5436655848f4c1f205a
franklyn damaceno <frank-destro@outlook.com>
2022-07-21 17:40:22 -0300

    project created

---
+++ backend/src/test/java/com/devsuperior/Delivery/DeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.Delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
