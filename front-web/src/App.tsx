5a629e8c016119dc4ac8862b46fcb453e3d7087f
franklyn damaceno <frank-destro@outlook.com>
2022-07-24 16:45:28 -0300

    Add home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import Home from './Home';
 import Navbar from './Navbar';
 
 
 function App() {
   return (
-    <div className="App">
+    <>
    <Navbar />
-    </div>
+   <Home />
+    </>
   );
 }
 
