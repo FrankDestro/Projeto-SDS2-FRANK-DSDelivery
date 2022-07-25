280f8684159afa65ab8a211c95e79e9e16841dfd
franklyn damaceno <frank-destro@outlook.com>
2022-07-24 22:55:55 -0300

    products list backend created

--- front-web/src/Orders/StepsHeader.tsx
+++ front-web/src/Orders/StepsHeader.tsx
@@ -2,7 +2,7 @@
 function StepsHeader() {
     return (
         <header className='orders-steps-container'>
-            <div className='order-steps-content'>
+            <div className='orders-steps-content'>
                 <h1 className='steps-title'>
                     SIGA AS ETAPAS
                 </h1>
---
+++ front-web/src/api.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const API_URL = 'http://localhost:8080';
+
+export function fetchProducts(){
+    return axios(`${API_URL}/products`);
+
+}
