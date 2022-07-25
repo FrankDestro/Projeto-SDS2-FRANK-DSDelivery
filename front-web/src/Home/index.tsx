808b45ac0de7ec7034376eed59d30c375f1fa5e0
franklyn damaceno <frank-destro@outlook.com>
2022-07-24 21:29:17 -0300

    StepsHeader

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 import { ReactComponent as MainImage } from './main.svg';
 import Footer from '../Footer';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
@@ -15,7 +16,7 @@ function Home() {
                             Escolha o seu pedido e em poucos minutos  <br />
                             levaremoss na sua porta
                         </h3>
-                        <a href="orders" className="home-btn-order">
+                        <a href="/orders" className="home-btn-order">
                             FAZER PEDIDO
                         </a>
                     </div>
